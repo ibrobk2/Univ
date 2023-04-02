45c28f7f48bd54356a2dac9748f48436d4df3437
Ibrahim Yusuf <ibrobk@gmail.com>
2023-04-02 16:14:49 +0100

    Loop Fixed

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let univ = {
-        getUniv : function(country){
-        fetch("http://universities.hipolabs.com/search?country="+country)
-        .then((response) => response.json())
-        .then((result) => console.log(result));
-    },
-    displayUniv : function(result){
+    getData :function (country) {fetch("http://universities.hipolabs.com/search?country="+country)
+    .then((resp)=>resp.json())
+    .then((data)=>this.displayUniv(data))
+},
 
-    },
+    displayUniv : function (data) {
+       
+         for(let i=0; i<data.length; i++){
+            let x = data[i];
+            document.querySelector('.result').innerHTML = x.name;
+            document.querySelector('p').innerText = x.web_pages;
+            
+            } 
+
+
+
+    
+        
+
 }
 
-document.querySelector('.search-btn button').addEventListener('click', function(){
-    var x = document.querySelector('.search-input').value;
-    univ.getUniv(x);
-});
+
+
+}
